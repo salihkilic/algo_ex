b255e9f8351f4c3da7521151479d960b58a5fd78
Salih Kilic <info@importantpeople.nl>
2026-01-26 18:24:05 +0100

    Added FloydWarshall

---
+++ _12_FloydWarshall/Floyd-Warshall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _12_FloydWarshall;
+
+public class FloydWarshall
+{
+    /// <summary>
+    /// Initializes the distance and next node matrices for the Floyd-Warshall algorithm.
+    /// </summary>
+    /// <param name="graph">The input adjacency matrix where graph[i,j] represents the weight of the edge from i to j. 
+    /// Infinity indicates no edge.</param>
+    /// <returns>
+    /// A Tuple containing:
+    /// Item1 (double[,]): The initial distance matrix. Should be a copy of the graph, but with 0 on the diagonal.
+    /// Item2 (int[,]): The initial 'next' node matrix for path reconstruction.
+    ///                 If there is an edge from i to j, next[i,j] = j.
+    ///                 If there is no edge (infinity), next[i,j] = -1.
+    ///                 next[i,i] should probably be -1 or i depending on convention, usually -1 for path reconstruction stop.
+    /// </returns>
+    public static Tuple<double[,], int[,]> Init(double[,] graph)
+    {
+        //ToDo 1.1 Initialize the distance and next matrix
+        throw new NotImplementedException();
+    }
+    
+    /// <summary>
+    /// Executes the Floyd-Warshall algorithm to find the All-Pairs Shortest Paths.
+    /// </summary>
+    /// <param name="graph">The adjacency matrix of the graph.</param>
+    /// <returns>
+    /// A Tuple containing:
+    /// Item1 (double[,]): The matrix of shortest distances between every pair of nodes.
+    /// Item2 (int[,]): The matrix of 'next' nodes to reconstruction the shortest paths.
+    /// </returns>
+    public static Tuple<double[,], int[,]> AllPairShortestPath(double[,] graph)
+    {
+        //ToDo 1: Floyd-Warshall Algorithm, All Pairs Shortest Path
+        throw new NotImplementedException();
+        //Includes 1.1
+        var (dist, next) = Init(graph);
+    }
+}
---
+++ _12_FloydWarshall/Program.cs
@@ -0,0 +1,97 @@
+﻿using AlgorithmTestFramework;
+
+namespace _12_FloydWarshall;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("--- Floyd-Warshall Algorithm Exercise Runner --- \n");
+        RunTests();
+    }
+
+    private static void RunTests()
+    {
+        Console.WriteLine("Testing Initialization...");
+        TestRunner.RunTest("Init Matrix Setup", TestInit, 
+            "Init should create the initial Distance and Next matrices correctly based on the input graph.");
+
+        Console.WriteLine("\nTesting All-Pairs Shortest Path...");
+        TestRunner.RunTest("Floyd-Warshall Execution", TestAllPairShortestPath, 
+            "The algorithm should correctly compute the shortest paths between all pairs of nodes.");
+    }
+
+    private static void TestInit()
+    {
+        double inf = double.PositiveInfinity;
+        
+        // Input graph
+        double[,] graph = {
+            {inf,   3, inf,   5},
+            { 2 , inf, inf, inf},
+            {inf,   7, inf,   1},
+            {inf, inf,   6, inf}
+        };
+
+        // Expected Distance Matrix (Init state)
+        // Should indicate direct edge weights
+        double[,] expectedDistances = {
+            {  0,   3, inf,   5},
+            {  2,   0, inf, inf},
+            {inf,   7,   0,   1},
+            {inf, inf,   6,   0}
+        };
+
+        // Expected Next Matrix (Init state)
+        // If edge exists i->j, next is j. Else -1 (or some indicator).
+        int[,] expectedNextNodes = {
+            {-1,  1, -1,  3},
+            { 0, -1, -1, -1},
+            {-1,  1, -1,  3},
+            {-1, -1,  2, -1}
+        };
+
+        var result = FloydWarshall.Init(graph);
+        
+        // Check Distances
+        Assertions.Assert2DEqual(result.Item1, expectedDistances);
+        
+        // Check Next Nodes
+        Assertions.Assert2DEqual(result.Item2, expectedNextNodes);
+    }
+
+    private static void TestAllPairShortestPath()
+    {
+        double inf = double.PositiveInfinity;
+        double[,] graph = {
+            {inf,   3, inf,   5},
+            { 2 , inf, inf, inf},
+            {inf,   7, inf,   1},
+            {inf, inf,   6, inf}
+        };  
+
+        // Final Optimized Distances
+        double[,] expectedDistances = {
+            { 0,  3, 11, 5},
+            { 2,  0, 13, 7},
+            { 9,  7,  0, 1},
+            {15, 13,  6, 0}
+        };
+
+        // Final Next Nodes for path reconstruction
+        int[,] expectedNextNodes = {
+            {-1,  1,  3,  3},
+            { 0, -1,  0,  0},
+            { 1,  1, -1,  3},
+            { 2,  2,  2, -1}
+        };
+
+        var result = FloydWarshall.AllPairShortestPath(graph);
+
+        // Check Distances
+        Assertions.Assert2DEqual(result.Item1, expectedDistances);
+
+        // Check Next Nodes
+        Assertions.Assert2DEqual(result.Item2, expectedNextNodes);
+    }
+}
