1fb5768d21af36b8b940436e76e2400ac3bbdfdc
Thomas <tmc.heuvelman@gmail.com>
2026-04-14 20:43:13 +0200

    2D arrays also finito

--- _01_Arrays/Array2D.cs
+++ _01_Arrays/Array2D.cs
@@ -11,8 +11,18 @@ public class Array2D
     // Input: {{1, 2}, {3, 4}}
     // Output: {3, 7}
     public static T[]? RowSum<T>(T[,] arr2D) where T : INumber<T>
-    {   
-        throw new NotImplementedException();
+    {
+        T[]? result = new T[arr2D.GetLength(0)];
+        for(int i = 0; i < arr2D.GetLength(0); i++)
+        {
+            T tmp = arr2D[i, 0];
+            for (int j = 1; j < arr2D.GetLength(1); j++)
+            {
+                tmp += arr2D[i, j];
+            }
+            result[i] = tmp;
+        }
+        return result;
     }
     
     // TODO: Calculate the sum of each column in the 2D array.
@@ -25,6 +35,16 @@ public class Array2D
     // Output: {4, 6}
     public static T[]? ColSum<T>(T[,] arr2D) where T : INumber<T>
     {
-        throw new NotImplementedException();
+        T[]? result = new T[arr2D.GetLength(1)];
+        for (int i = 0; i < arr2D.GetLength(1); i++)
+        {
+            T tmp = arr2D[0, i];
+            for (int j = 1; j < arr2D.GetLength(0); j++)
+            {
+                tmp += arr2D[j, i];
+            }
+            result[i] = tmp;
+        }
+        return result;
     }
 }
